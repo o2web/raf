9556f025f957132ac53c91c92987fd732873f53d
Charles Thibault <cthibault@o2web.ca>
2015-02-03 11:37:08 -0500

    update raf.js : pointerevents + fix minor bugs

--- demo/assets/js/demo.js
+++ demo/assets/js/demo.js
@@ -1,20 +1,21 @@
 jQuery(document).ready(function($){
 
 
+
   // générer des cercles, parce que les placer à la main c'est trop long
   var $circles = $('.demo-parallax .circles');
-  var count = 100;
+  var count = 60;
   var colors = ['turquoise','blue','violet'];
   var zMin = -5;
   var zMax = 5;
+  var containerH = $circles.height();
+  var containerW = $circles.width();
   for(var i=0; i<count; i++){
     var $circle = $('<div class="circle"></div>');
     $circle.addClass( colors[Math.floor(Math.random()*(colors.length))] );
     $circle.attr('z', Math.ceil(Math.random()* (zMax-zMin))+zMin );
-    $circle.css({
-      top: Math.round(Math.random()*$circles.height())+'px',
-      left: Math.round(Math.random()*$circles.width())+'px'
-    });
+    $circle[0].style.bottom = Math.round(Math.random()*containerH)+'px';
+    $circle[0].style.left = Math.round(Math.random()*containerW)+'px';
     $circles.append($circle);
   }
 
@@ -24,6 +25,7 @@ jQuery(document).ready(function($){
   	for(var i=0; i<codes.length; i++){
   		eval($(codes[i]).html());
   	}
+
   });
 
 });
--- raf.js
+++ raf.js
@@ -2,14 +2,16 @@
 // o2web.ca
 // 2015
 
-// RAF polyfill
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-// requestAnimationFrame polyfill by Erik Möller
-// fixes from Paul Irish and Tino Zijdel
-
 (function($) {
 
+  // jquery window
+  var $win = $(window);
+
+  // RAF polyfill
+  // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+  // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+  // requestAnimationFrame polyfill by Erik Möller
+  // fixes from Paul Irish and Tino Zijdel
   var lastTime = 0;
   var vendors = ['ms', 'moz', 'webkit', 'o'];
   for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
@@ -33,8 +35,13 @@
       clearTimeout(id);
     };
 
+
+  //
+  //
+  // HELPERS
+
   // count keys in object array
-  function count(obj){
+  count = function(obj){
     var count = 0;
     for(var key in obj)
       if (obj.hasOwnProperty(key))
@@ -43,33 +50,73 @@
   }
 
   // unset key in array
-  function unset(key, array){
+  unset = function(key, array){
+    if(typeof key == 'string'){
+      var value = array[key];
+      delete array[key];
+      return value;
+    }
     return array.splice(key, 1);
   }
 
+  // get dom element/prop combo
+  getDomPropCombo = function(elements, prefixes){
+    var test = 0;
+    var result = {
+      element: undefined,
+      prefix: undefined
+    }
+    for(var e=0; e<elements.length; e++)
+      for(var p=0; p<prefixes.length; p++)
+        if( prefixes[p]+'Height' in elements[e] && elements[e][prefixes[p]+'Height'] > test ){
+          test = elements[e][prefixes[p]+'Height'];
+          result = {
+            element: elements[e],
+            prefix: prefixes[p]
+          }
+        }
+    return result;
+  }
+
   // find hook inside an event's array
-  function findHook(hook, event){
+  findHook = function(hook, event){
     var key = -1;
     for(var i=0; i<event.length; i++)
       if(event[i].delegate == hook.delegate && event[i].callback == hook.callback) key = i;
     return key;
   }
 
-  // jquery window
-  $win = $(window);
+  // trigger hooks for an event
+  triggerHooks = function(event, data){
+    if(!event) return false;
+    if(data && typeof data != 'object') data = false;
+    for(var e=0; e<event.length; e++){
+      var hook = event[e];
+      if(hook.callback){
+        if(data) hook = $.extend(data, hook);
+        hook.callback(hook);
+      }
+    }
+  }
 
+  // track pointer position on mouse/touch move
+  trackPointer = function(e){
+    e.data.self.newPointer = { x: e.pageX, y: e.pageY };
+  }
 
   //
   //
-  // EVENT OBJECT
+  // HOOK OBJECT
   function hookObj(){
-    var hook = {};
-    hook.event = false;
-    hook.callback = false;
-    hook.data = false;
-    hook.delegate = $win;
+    // create empty hook
+    var hook = {
+      event: false,
+      callback: false,
+      data: false,
+      delegate: $win
+    };
 
-    // parse arguments
+    // parse arguments based on their types
     for(var i=0; i<arguments.length; i++){
       var arg = arguments[i];
       var type = typeof arg;
@@ -91,6 +138,8 @@
     //
     // SETUP
     var self = this;
+    var body = document.body;
+    var html = document.documentElement;
     // events holder
     this.events = [];
     // raf request
@@ -100,32 +149,137 @@
       top: window.pageYOffset,
       left: window.pageYOffset
     };
+    // window data
+    this.win = $.extend(
+      getDomPropCombo(
+        [window, document.documentElement], // EX. element: document.body
+        ['inner', 'client'] // EX. prefix: 'inner'
+      ),
+      {
+        width: 0,
+        height: 0,
+      }
+    );
+    // document data
+    this.doc = $.extend(
+      getDomPropCombo(
+        [document.body, document.documentElement], // EX. element: document.body
+        ['inner', 'client'] // EX. prefix: 'inner'
+      ),
+      {
+        width: 0,
+        height: 0,
+      }
+    );
     // pointer data
-    this.pointer = {
-      x: 0,
-      y: 0
+    this.pointer = {x:0, y:0};
+
+    //
+    //
+    // init functions triggered for the first event of a kind
+    this.inits = {
+      // init afterdocumentresize
+      afterdocumentresize: function(){
+        self.on('documentresize', $(self));
+      },
+      // init pointermove
+      pointermove: function(){
+        // start tracking pointer position on window
+        $win.on('mousemove touchmove', {self:self}, trackPointer);
+      },
     }
 
-    // detections
+    //
+    //
+    // kill functions triggered when there's no more hooks for an event
+    this.kills = {
+      // kill afterdocumentresize
+      afterdocumentresize: function(){
+        self.off('documentresize', $(self));
+      },
+      // kill pointerpove
+      pointermove: function(){
+        // stop tracking pointer position on window
+        $win.off('mousemove touchmove', trackPointer);
+      }
+    }
+
+    //
+    //
+    // detection for events
     this.detect = {
       // detect if scroll position has changed
-      scroll: function(e){
+      scroll: function(){
         var current = {
           top: window.pageYOffset,
           left: window.pageXOffset
         }
         // exit if scroll positions have not changed
         if(current.top == self.scroll.top && current.left == self.scroll.left) return;
-        // loop throught hooks on scroll event
-        for(var i=0; i<self.events.scroll.length; i++){
-          var hook = self.events.scroll[i]
-          hook.callback(hook);
-        }
+        // set new scroll positions
         self.scroll = current;
+        // loop throught hooks on scroll event
+        triggerHooks(self.events.scroll);
       },
-      // detect if pointer has moved
-      pointermove: function(e){
 
+      // detect if WINDOW size have changed
+      resize: function(){
+        var current = {
+          width: self.win.element[self.win.prefix+'Width'],
+          height: self.win.element[self.win.prefix+'Height']
+        }
+        // exit if window size have not changed
+        if(current.width == self.win.width && current.height == self.win.height) return;
+        // set new window sizes
+        self.win.width = current.width;
+        self.win.height = current.height;
+        // loop throught hooks on resize event
+        triggerHooks(self.events.resize);
+      },
+
+      // detect if DOCUMENT size have changed
+      documentresize: function(){
+        var current = {
+          width: self.doc.element[self.doc.prefix+'Width'],
+          height: self.doc.element[self.doc.prefix+'Height']
+        }
+        // exit if window size have not changed
+        if(current.width == self.doc.width && current.height == self.doc.height){
+          if(!self.doc.last) return;
+          self.doc.last = undefined;
+          triggerHooks(self.events.afterdocumentresize);
+          return;
+        }
+        // set last
+        self.doc.last = true;
+        // set new document sizes
+        self.doc.width = current.width;
+        self.doc.height = current.height;
+        // loop throught hooks on resize event
+        triggerHooks(self.events.documentresize);
+      },
+
+      // detect if pointer has moved
+      pointermove: function(){
+        // exit here if there is no pointer new position
+        if(!self.newPointer) return;
+        // exit if position is the same
+        if( self.pointer.x == self.newPointer.x && self.pointer.y == self.newPointer.y ) return;
+        // set new pointer position and erase old one
+        self.pointer = self.newPointer;
+        self.newPointer = undefined;
+        // loop throught hooks on pointermove event
+        triggerHooks(self.events.pointermove, {pointer:self.pointer});
+      },
+
+      // detect when next frame is rendered
+      nextframe: function(){
+        // loop throught hooks on nextframe event
+        triggerHooks(self.events.nextframe);
+        // unset nextframe event, so it runs only once
+        unset('nextframe', self.events);
+        // update count
+        self.eventsCount = count(self.events);
       }
     }
 
@@ -138,13 +292,15 @@
       // parse arguments into a new event
       var hook = hookObj.apply(this, arguments);
       // exit if event type is not set
-      if(!hook.event) return false;
+      if(!hook.event) return self;
       // create / append hook
       if(!self.events[hook.event]){
         // create new event
         self.events[hook.event] = [];
         // update count
         self.eventsCount = count(self.events);
+        // if an init function exists for this event, trigger it;
+        if(self.inits[hook.event]) self.inits[hook.event]();
       }
       // push hook into event
       self.events[hook.event].push(hook);
@@ -162,7 +318,7 @@
       // parse arguments into a new event
       var hook = hookObj.apply(this, arguments);
       // exit if event is not found
-      if(!self.events[hook.event]) return false;
+      if(!self.events[hook.event]) return self;
       // get event for this hook
       var event = self.events[hook.event];
       // remove whole event if is not set for a specific selection
@@ -172,9 +328,14 @@
         // get hook index
         var hookIndex = findHook(hook, event);
         // if hook is found, unset it
-        if(hookIndex>0) unset(hookIndex, event);
+        if(hookIndex>-1) unset(hookIndex, event);
         // if event is empty, unset it
-        if(event.length==0) unset(hook.event, self.events);
+        if(event.length==0){
+          // unset event
+          unset(hook.event, self.events);
+          // if a kill function exists fot this event, trigger it;
+          if(self.kills[hook.event]) self.kills[hook.event]();
+        }
       }
       // update count
       self.eventsCount = count(self.events);
@@ -191,8 +352,7 @@
       if(!self.eventsCount) return self.stop();
       // parse each event
       for(var e in self.events){
-        if(e=='scroll') self.detect.scroll(e);
-        if(e=='pointermove') self.detect.pointermove(e);
+        if(self.detect[e]) self.detect[e]();
       }
       // request another frame
       self.request = window.requestAnimationFrame(self.loop);
